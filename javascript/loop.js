d09ff6c8c9a40298fe8b0fc5f56e12f8aacc66a6
bhadreshce <bhadreshce17@gmail.com>
2024-11-25 09:54:22 +0530

    ad

---
+++ javascript/loop.js
@@ -0,0 +1,91 @@
+// loop is use for reapeated statment
+// Loops in programming are control flow statements that allow you to repeatedly execute a block of code until a certain condition is met.
+// for (var i = 0; i < 5; i++) {
+
+//     console.log(i);
+// }
+
+// var sum = 0;
+// for (var i = 0; i <= 50; i++) {
+//     sum += i
+// }
+
+// console.log(sum);
+
+// 2
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log('2 * ' + i + ' = ' + 2 * i);
+// }
+
+// *
+// **
+// ***
+// ****
+// *****
+// var str = ''
+// for (var i = 1; i <= 10; i++) {
+//     for (var j = 1; j <= i; j++) {
+//         str += '*'
+//         // *
+//         // **
+//         // ***
+//         // ****
+//         // *****
+
+//     }
+//     console.log(str);
+//     str = ''
+
+// }
+
+
+// A while loop is a programming construct that repeats a set of instructions until a condition is met. It's commonly used when the exact number of times a code needs to be executed is unknown
+
+// var i = 11;
+
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+
+// }
+
+
+// do ....while : you can print atleast one
+// var i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i < 10);
+
+//  1
+//  22
+//  333
+//  4444
+//  55555
+
+// 1
+// 12
+// 123
+// string + number = string
+
+console.log('1' + 1);
+var str = 0
+for (var i = 1; i <= 10; i++) {
+    for (var j = 1; j <= i; j++) {
+        str += j
+        // *
+        // **
+        // ***
+        // ****
+        // *****
+
+    }
+    console.log(str);
+    str = 0
+
+}
+
+
+
+
